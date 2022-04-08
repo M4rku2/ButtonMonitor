6e2c37dc596dc5e6a0c8104e62f3c406ec004bfd
markus.schroettner <markus.schroettner@bulme.at>
2022-04-08 12:15:13 +0200

    Endversion

--- gpio.cpp
+++ gpio.cpp
@@ -11,12 +11,14 @@ gpio::gpio(QObject *parent) : QObject(parent)
     for (auto pin : LEDS) // Outputs
         lgGpioClaimOutput(m_handle, LFLAGS, pin, init_level);
     for (auto pin : BUTTONS) // Inputs
+    {
         lgGpioClaimInput(m_handle, LFLAGS, pin);
+        m_oldStates.push_back((bool)lgGpioRead(m_handle, pin)); // ensures proper edge-detection from point of initialization on
+    }
 }
 
 gpio::~gpio()
 {
-    // Cleanup GPIO
     int init_level = 0;
     for (auto pin : LEDS)
         lgGpioWrite(m_handle, pin, init_level);
@@ -24,7 +26,6 @@ gpio::~gpio()
     qDebug() << "bye";
 }
 
-// Write to pins
 void gpio::set(int pin, bool value)
 {
     lgGpioWrite(m_handle, pin, value);
@@ -42,7 +43,34 @@ void gpio::set(unsigned int pattern)
 }
 
 // Read pin state
-bool gpio::get(int pin)
+bool gpio::get(int pin, getMode mode)
 {
-    return lgGpioRead(m_handle, pin);
+    unsigned int idx = BUTTONS.indexOf(pin);
+
+    bool pinState = lgGpioRead(m_handle, pin);
+    bool out = pinState;
+
+    switch(mode)
+    {
+
+    case getMode::falling:
+    {
+        out = (m_oldStates[idx] && !pinState);  // ⁻⁻\__
+        m_oldStates[idx] = pinState;
+        break;
+    }
+
+    case getMode::rising:
+    {
+        out = (!m_oldStates[idx] && pinState);  // __/⁻⁻
+        m_oldStates[idx] = pinState;
+        break;
+    }
+
+    default: break;    // ^= getMode::raw
+
+    }
+
+    return out;
 }
+
